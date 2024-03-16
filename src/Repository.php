be9e65abc7204c9d7fccdae1b550b5fab6adf883
covaleski <covaleski23@gmail.com>
2024-03-16 10:40:29 -0300

    feat: create `Repository`

---
+++ src/Repository.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * This file is part of Laucov's Dependency Injection Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package injection
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Injection;
+
+use Laucov\Injection\Interfaces\DependencyInterface;
+
+/**
+ * Stores dependency sources.
+ */
+class Repository
+{
+    /**
+     * Registered dependencies.
+     * 
+     * @var array<string, DependencyInterface>
+     */
+    protected array $dependencies = [];
+
+    /**
+     * Get a dependency value.
+     */
+    public function getValue(string $name): mixed
+    {
+        return $this->dependencies[$name]->get();
+    }
+
+    /**
+     * Set a factory function to get dependencies from.
+     */
+    public function setFactory(string $name, callable $factory): static
+    {
+        $this->dependencies[$name] = new FactoryDependency($factory);
+        return $this;
+    }
+
+    /**
+     * Set an iterable dependency.
+     */
+    public function setIterable(string $name, iterable $iterable): static
+    {
+        $this->dependencies[$name] = new IterableDependency($iterable);
+        return $this;
+    }
+
+    /**
+     * Set a value dependency.
+     */
+    public function setValue(string $name, mixed $value): static
+    {
+        $this->dependencies[$name] = new ValueDependency($value);
+        return $this;
+    }
+}
---
+++ tests/RepositoryTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * This file is part of Laucov's Dependency Injection Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package injection
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Laucov\Injection\Repository;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Laucov\Injection\Repository
+ */
+class RepositoryTest extends TestCase
+{
+    protected Repository $repo;
+
+    /**
+     * @covers ::getValue
+     * @covers ::setFactory
+     * @covers ::setIterable
+     * @covers ::setValue
+     * @uses Laucov\Injection\FactoryDependency::__construct
+     * @uses Laucov\Injection\FactoryDependency::get
+     * @uses Laucov\Injection\IterableDependency::__construct
+     * @uses Laucov\Injection\IterableDependency::get
+     * @uses Laucov\Injection\ValueDependency::__construct
+     * @uses Laucov\Injection\ValueDependency::get
+     */
+    public function testCanSetDependencies(): void
+    {
+        // Set absolute values.
+        $this->repo
+            ->setValue('string', 'John')
+            ->setValue('int', 42);
+        $this->assertSame('John', $this->repo->getValue('string'));
+        $this->assertSame(42, $this->repo->getValue('int'));
+
+        // Set iterable values.
+        $this->repo->setIterable('float', [0.00, 0.25, 0.50, 0.75, 1.00]);
+        $this->assertSame(0.00, $this->repo->getValue('float'));
+        $this->assertSame(0.25, $this->repo->getValue('float'));
+        $this->assertSame(0.50, $this->repo->getValue('float'));
+        $this->assertSame(0.75, $this->repo->getValue('float'));
+        $this->assertSame(1.00, $this->repo->getValue('float'));
+        $this->assertSame(0.00, $this->repo->getValue('float'));
+        $this->assertSame(0.25, $this->repo->getValue('float'));
+
+        // Set factory function.
+        // Also test if can replace a type in the repository (replacing `int`).
+        $factory = fn () => ValueHolderA::$value++;
+        $this->repo->setFactory('int', $factory);
+        $this->assertSame(0, $this->repo->getValue('int'));
+        $this->assertSame(1, $this->repo->getValue('int'));
+        $this->assertSame(2, $this->repo->getValue('int'));
+    }
+
+    protected function setUp(): void
+    {
+        $this->repo = new Repository();
+    }
+}
+
+class ValueHolderA
+{
+    public static int $value = 0;
+}
